8084ec97e00522a63bc3d625d94869e29ad087a3
ChizarR <chizarr27@gmail.com>
2023-02-18 18:06:35 +0100

    init

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require("st.base")
+require("st.maps")
+require("st.plugins")
+require("st.colorscheme")
+
+require("st.after.cmp")
+require("st.after.lsp")
+
+require("st.after.treesitter")
+require("st.after.indentline")
+require("st.after.nvimtree")
