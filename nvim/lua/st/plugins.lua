cbb4fa074fda5977a4d9b3ea89d3aad5e4488d9a
ChizarR <chizarr27@gmail.com>
2023-02-26 11:36:55 +0100

    new plugs

--- nvim/lua/st/plugins.lua
+++ nvim/lua/st/plugins.lua
@@ -98,6 +98,9 @@ return packer.startup(function(use)
   -- Nvimtree
   use "nvim-tree/nvim-tree.lua"
 
+  -- Maps helpers
+  use "folke/which-key.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
